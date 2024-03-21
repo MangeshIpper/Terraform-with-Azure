b720230611425207293086a0297bbcada7d1eea9
MangeshIpperNCI <mangesh.ipper@gmail.com>
2024-03-21 12:08:27 +0000

    Creating subnets using locals.

---
+++ 6. Another way of creating the subnets/main.tf
@@ -0,0 +1,82 @@
+/*
+
+The following links provide the documentation for the new blocks used
+in this terraform configuration file
+
+1.azurerm_subnet - https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/subnet
+
+*/
+
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.96.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  subscription_id = "dc5e1966-3973-4fa0-ac51-28dfbbc03411"
+  tenant_id = "5de110f8-2e0f-4d45-891d-bcf2218e253d"
+  client_id = "95648796-9e45-4bbf-bc21-a0e74fcaf3c5"
+  client_secret = "EG.8Q~tpJZCySOtOCV50lFQEaPZc7fX5N213-bTw"
+  features {}
+}
+
+locals {
+  resource_group_name="app-grp"
+  location="East US 2"
+  virtual_network={
+    name="app-network"
+    address_space="10.0.0.0/16"
+  }
+
+  subnets=[
+    {
+      name="subnetA"
+      address_prefix="10.0.0.0/24"
+    },
+    {
+      name="subnetB"
+      address_prefix="10.0.1.0/24"
+    }
+  ]
+}
+
+resource "azurerm_resource_group" "appgrp" {
+  name     = local.resource_group_name
+  location = local.location  
+}
+
+resource "azurerm_virtual_network" "appnetwork" {
+  name                = local.virtual_network.name
+  location            = local.location
+  resource_group_name = local.resource_group_name
+  address_space       = [local.virtual_network.address_space]  
+  
+   depends_on = [
+     azurerm_resource_group.appgrp
+   ]
+  }
+
+
+  resource "azurerm_subnet" "subnetA" {
+  name                 = local.subnets[0].name
+  resource_group_name  = local.resource_group_name
+  virtual_network_name = local.virtual_network.name
+  address_prefixes     = [local.subnets[0].address_prefix]
+  depends_on = [
+    azurerm_virtual_network.appnetwork
+  ]
+}
+
+resource "azurerm_subnet" "subnetB" {
+  name                 = local.subnets[1].name
+  resource_group_name  = local.resource_group_name
+  virtual_network_name = local.virtual_network.name
+  address_prefixes     = [local.subnets[1].address_prefix]
+  depends_on = [
+    azurerm_virtual_network.appnetwork
+  ]
+}
