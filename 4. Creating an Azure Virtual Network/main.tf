cd3d6f43639d99b11b37a768e28224d3b0f49e26
MangeshIpperNCI <mangesh.ipper@gmail.com>
2024-03-21 11:56:05 +0000

    Created Virtual network.

---
+++ 4. Creating an Azure Virtual Network/main.tf
@@ -0,0 +1,48 @@
+/*
+
+The following links provide the documentation for the new blocks used
+in this terraform configuration file
+
+1.azurerm_virtual_network - https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/virtual_network
+
+*/
+
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.96.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  subscription_id = "dc5e1966-3973-4fa0-ac51-28dfbbc03411"
+  tenant_id = "5de110f8-2e0f-4d45-891d-bcf2218e253d"
+  client_id = "95648796-9e45-4bbf-bc21-a0e74fcaf3c5"
+  client_secret = "EG.8Q~tpJZCySOtOCV50lFQEaPZc7fX5N213-bTw"
+  features {}
+}
+
+resource "azurerm_resource_group" "appgrp" {
+  name     = "app-grp"
+  location = "East US 2"
+}
+
+resource "azurerm_virtual_network" "appnetwork" {
+  name                = "app-network"
+  location            = "East US 2"
+  resource_group_name = "app-grp"
+  address_space       = ["10.0.0.0/16"]
+
+  subnet {
+    name           = "subnetA"
+    address_prefix = "10.0.0.0/24"
+  }
+
+  subnet {
+    name           = "subnetB"
+    address_prefix = "10.0.1.0/24"    
+  }
+}
+
